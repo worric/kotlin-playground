dcac6a7c04dfd0932fef1132d448b1cd1f20849f
Robin Damsgaard Larsen <rdalarsen@gmail.com>
2018-08-13 22:58:11 +0200

    misc: Initialize recycerview and add adapter

--- app/src/main/java/me/worric/kotlinplayground/MainActivity.kt
+++ app/src/main/java/me/worric/kotlinplayground/MainActivity.kt
@@ -2,17 +2,31 @@ package me.worric.kotlinplayground
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val items = listOf(
+            "Mon 6/23 - Sunny - 31/17",
+            "Tue 6/24 - Foggy - 21/8",
+            "Wed 6/25 - Cloudy - 22/17",
+            "Thurs 6/26 - Rainy - 18/11",
+            "Fri 6/27 - Foggy - 21/10",
+            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+            "Sun 6/29 - Sunny - 20/7"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val forecastList = findViewById<RecyclerView>(R.id.rv_forecast_list)
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
+
         val person = Person(name = "John", surname = "Smith")
-        message.text = person.getNameFullName()
         toast(message = person.getNameFullName())
     }
 
