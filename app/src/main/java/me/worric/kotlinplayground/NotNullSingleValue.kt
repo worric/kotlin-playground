6c53896ebfad1c6b23999fc4502448c1fecd72e8
Robin Damsgaard Larsen <rdalarsen@gmail.com>
2018-09-04 22:53:40 +0200

    misc: Add custom delegate class

--- app/src/main/java/me/worric/kotlinplayground/NotNullSingleValue.kt
+++ app/src/main/java/me/worric/kotlinplayground/NotNullSingleValue.kt
@@ -1,4 +1,22 @@
 package me.worric.kotlinplayground
 
-class NotNullSingleValue {
+import kotlin.reflect.KProperty
+
+class NotNullSingleValue<T> {
+
+    private var value: T? = null
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        return value ?: throw IllegalStateException("${property.name} not initialized")
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        this.value = if (this.value == null) value
+        else throw IllegalStateException("${property.name} already initialized")
+    }
+
+    object DelegatesExt {
+        fun <T> notNullSingleValue() = NotNullSingleValue<T>()
+    }
+
 }
