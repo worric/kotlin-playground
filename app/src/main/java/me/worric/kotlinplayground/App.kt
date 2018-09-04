2baed3b73c5563bce424fe8f479bc8d2fcfafe39
Robin Damsgaard Larsen <rdalarsen@gmail.com>
2018-09-04 22:40:04 +0200

    misc: Add application class

---
+++ app/src/main/java/me/worric/kotlinplayground/App.kt
@@ -0,0 +1,17 @@
+package me.worric.kotlinplayground
+
+import android.app.Application
+
+class App : Application() {
+
+    companion object {
+        private var instance: Application? = null
+        fun instance() = instance!!
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+}
